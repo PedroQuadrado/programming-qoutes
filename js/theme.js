05c5376908d19b1606c1c1d537fceeac0051349e
PedroQuadrado <51176756+PedroQuadrado@users.noreply.github.com>
2020-10-09 22:32:23 +0100

    set theme

--- js/theme.js
+++ js/theme.js
@@ -1,25 +1,26 @@
 let theme = localStorage.theme;
 const switchBtn = document.getElementById("switch");
 
-if (theme === "light") {
-    document.documentElement.setAttribute("data-theme", "light");
-} else if (theme === "dark") {
-    document.documentElement.setAttribute("data-theme", "dark");
-} else {
-    localStorage.setItem("theme", "light");
+if (theme == undefined) {
+  localStorage.setItem("theme", "light");
 }
-
-function changeTheme() {
+switchBtn.onclick = function () {
+    let theme = localStorage.theme;
     if (theme === "light") {
         setTheme("dark", 1);
-    } else if (theme === "dark") {
+      } else if (theme === "dark") {
         setTheme("light", 0);
-    }
+      }
+};
+
+if (theme === "light") {
+  document.documentElement.setAttribute("data-theme", "light");
+} else if (theme === "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
 }
 
 function setTheme(themeName, deg) {
-    document.documentElement.setAttribute("data-theme", themeName);
-    theme = themeName;
-    localStorage.setItem("theme", themeName);
-    switchBtn.style.transform = "rotate(" + 180 * deg + "deg)"
+  document.documentElement.setAttribute("data-theme", themeName);
+  localStorage.setItem("theme", themeName);
+  switchBtn.style.transform = "rotate(" + 180 * deg + "deg)";
 }
